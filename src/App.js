36cc7e88b1e116b40ab3e3085bff5246dbbe604a
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-07-12 15:36:09 -0500

    Add Course Route to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import HomeScreen from './pages/home/HomeScreen';
 import LoginScreen from './pages/login/LoginScreen';
 import HomeStudent from './pages/student/home/HomeStudent';
+import CourseScreen from './pages/course/CourseScreen';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" component={HomeScreen} exact />
         <Route path="/login" component={LoginScreen} exact />
         <Route path="/student" component={HomeStudent} exact />
+        <Route path="/course/:courseid" component={CourseScreen} exact />
       </>
     </Router>
   );
