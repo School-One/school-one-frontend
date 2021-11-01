bef75f10b71c4874c26a03c9127a327b6219233f
Marcelo Arias <hola@marceloarias.com>
2021-11-01 15:12:31 -0500

    Update Login design

--- src/pages/login/LoginScreen.jsx
+++ src/pages/login/LoginScreen.jsx
@@ -9,117 +9,10 @@ import { AuthContext } from "../../context/auth";
 //COMPONENTS
 import LoadingBox from "../../components/main/loadingBox/LoadingBox";
 import MessageBox from "../../components/main/messageBox/MessageBox";
-import ImageSVG from "../../img/img.svg";
+import CollaborationImage from "../../img/real_time_collaboration.svg";
+import styles from "./Login.module.css";
 
 const LoginStyled = styled.div`
-    .body-login {
-        font-family: "Poppins", sans-serif !important;
-        min-height: 100vh;
-        width: 100%;
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-    }
-    section {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .main {
-        padding: 0px !important;
-    }
-    .loginxcontainer {
-        max-width: 450px;
-        padding: 24px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .title {
-        text-transform: uppercase;
-        font-size: 3em;
-        font-weight: bold;
-        text-align: center;
-        letter-spacing: 1px;
-    }
-
-    .separator {
-        width: 150px;
-        height: 4px;
-        background-color: #843bc7;
-        margin: 24px;
-    }
-
-    .welcomexmessage {
-        text-align: center;
-        font-size: 1.1em;
-        line-height: 28px;
-        margin-bottom: 30px;
-        color: #696969;
-    }
-
-    .loginxform {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .formxcontrol {
-        width: 100%;
-        position: relative;
-        margin-bottom: 24px;
-        text-align: center;
-    }
-    .form_control_links {
-        display: flex;
-        justify-content: center;
-        gap: 15px;
-        text-align: center;
-    }
-    .input_login,
-    .button {
-        border: none;
-        outline: none;
-        border-radius: 30px;
-        font-size: 1.1em;
-    }
-    .input_login {
-        width: 100%;
-        background: #e6e6e6;
-        color: #333;
-        letter-spacing: 0.5px;
-        padding: 14px 64px;
-    }
-    .input_login ~ i {
-        position: absolute;
-        left: 32px;
-        top: 50%;
-        transform: translateY(-50%);
-        color: #888;
-        transition: color 0.4s;
-    }
-    .input_login:focus ~ i {
-        color: #843bc7;
-    }
-    .button.submit {
-        color: #fff;
-        padding: 14px 64px;
-        margin: 32px auto;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-weight: bold;
-        background-image: linear-gradient(to right, #8b33c5, #15a0e1);
-        cursor: pointer;
-        transition: opacity 0.4s;
-    }
-    .button.submit:hover {
-        opacity: 0.9;
-    }
-    .comeback {
-        align-items: center;
-        text-decoration: none !important;
-        color: #888;
-    }
     @media only screen and (max-width: 768px) {
         .side {
             display: none !important;
@@ -168,15 +61,19 @@ export default function LoginScreen(props) {
 
     return (
         <LoginStyled>
-            <div className="body-login">
-                <section className="side">
-                    <img src={ImageSVG} alt="a" className="sideimage" />
+            <div className={styles.bodyLogin}>
+                <section className={styles.side}>
+                    <img
+                        src={CollaborationImage}
+                        alt="a"
+                        className={styles.sideimage}
+                    />
                 </section>
 
-                <section className="main">
-                    <div className="loginxcontainer">
-                        <p className="title">Bienvenido de vuelta</p>
-                        <div className="separator" />
+                <section className={styles.main}>
+                    <div className={styles.loginxcontainer}>
+                        <p className={styles.title}>Bienvenido de vuelta</p>
+                        <div className={styles.separator} />
                         {loading ? (
                             <LoadingBox></LoadingBox>
                         ) : errors ? (
@@ -185,47 +82,55 @@ export default function LoginScreen(props) {
                             </MessageBox>
                         ) : (
                             <>
-                                <p className="welcomexmessage">
+                                <p className={styles.welcomexmessage}>
                                     Welcome to our E-learning :D
                                 </p>
                             </>
                         )}
 
-                        <form className="loginxform" onSubmit={submitHandler}>
-                            <div className="formxcontrol">
+                        <form
+                            className={styles.loginxform}
+                            onSubmit={submitHandler}
+                        >
+                            <div className={styles.formxcontrol}>
                                 <input
                                     type="text"
-                                    placeholder="Username"
+                                    placeholder="Usuario"
                                     value={email}
                                     onChange={(e) =>
                                         onChange(e.target.value, "email")
                                     }
-                                    className="input_login"
+                                    className={styles.input_login}
                                     autoComplete="true"
                                 />
                                 <i className="fa fa-user"></i>
                             </div>
-                            <div className="formxcontrol">
+                            <div className={styles.formxcontrol}>
                                 <input
                                     type="password"
-                                    placeholder="Password"
+                                    placeholder="Contraseña"
                                     value={password}
                                     onChange={(e) =>
                                         onChange(e.target.value, "password")
                                     }
-                                    className="input_login"
+                                    className={styles.input_login}
                                     autoComplete="true"
                                 />
                                 <i className="fa fa-lock"></i>
                             </div>
-                            <button className="button submit">Login</button>
+                            <button className={styles.buttonSubmit}>
+                                Login
+                            </button>
 
-                            <div className="form_control_links">
-                                <Link to="/" className="comeback">
+                            <div className={styles.form_control_links}>
+                                <Link to="/" className={styles.comeback}>
                                     Regresar
                                 </Link>
                                 |
-                                <Link to="/login-teacher" className="comeback">
+                                <Link
+                                    to="/login-teacher"
+                                    className={styles.comeback}
+                                >
                                     Soy profesor
                                 </Link>
                             </div>
