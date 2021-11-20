75c55c809915bcf10e2d6423ec5282c9498bbe02
Marcelo Arias <hola@marceloarias.com>
2021-11-20 00:28:09 -0500

    Remove Bootstrap CDN and add to dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import ApolloProvider from './ApolloProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
