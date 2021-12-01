d48b2435175fa318d25f4787d5c6fd89f4df13a7
Marcelo Arias <hola@marceloarias.com>
2021-12-01 09:46:53 -0500

    Remove BoxIcons CDN

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ApolloProvider from './ApolloProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons/css/boxicons.min.css';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
